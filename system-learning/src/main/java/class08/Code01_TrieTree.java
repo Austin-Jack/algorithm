e348b9d8f97225ea059117fdce9625536261310f
Austin-Jack <llynsyw@163.com>
2025-09-18 09:42:30 +0800

    前缀树练习

--- system-learning/src/main/java/class08/Code01_TrieTree.java
+++ system-learning/src/main/java/class08/Code01_TrieTree.java
@@ -7,15 +7,10 @@ public class Code01_TrieTree {
 
 	// 前缀树节点类型
 	public static class Node1 {
-		public int pass;
-		public int end;
-		public Node1[] nexts;
-
-		public Node1() {
-			pass = 0;
-			end = 0;
-			nexts = new Node1[26];
-		}
+		int pass;
+		int end;
+		
+		Node1[] nexts = new Node1[26];
 	}
 
 	public static class Trie1 {
@@ -26,15 +21,13 @@ public class Code01_TrieTree {
 		}
 
 		public void insert(String word) {
-			if (word == null) {
+			if (word == null || word.isEmpty()) {
 				return;
 			}
-			char[] chs = word.toCharArray();
 			Node1 node = root;
-			node.pass++;
-			int index = 0;
-			for (int i = 0; i < chs.length; i++) { // 从左往右遍历字符
-				index = chs[i] - 'a'; // 由字符，对应成走向哪条路
+		    char chs[] = word.toCharArray();
+			for (char ch : chs) {
+				int index = ch - 'a';
 				if (node.nexts[index] == null) {
 					node.nexts[index] = new Node1();
 				}
@@ -45,33 +38,31 @@ public class Code01_TrieTree {
 		}
 
 		public void delete(String word) {
-			if (search(word) != 0) {
-				char[] chs = word.toCharArray();
-				Node1 node = root;
-				node.pass--;
-				int index = 0;
-				for (int i = 0; i < chs.length; i++) {
-					index = chs[i] - 'a';
-					if (--node.nexts[index].pass == 0) {
-						node.nexts[index] = null;
-						return;
-					}
-					node = node.nexts[index];
-				}
-				node.end--;
+			if (search(word) == 0) {
+				return;
 			}
+			char[] chs = word.toCharArray();
+			Node1 node = root;
+			for (char ch : chs) {
+				int index = ch - 'a';
+				if (--node.nexts[index].pass == 0) {
+					node.nexts[index] = null;
+					return;
+				}
+				node = node.nexts[index];
+			}
+			node.end--;
 		}
 
 		// word这个单词之前加入过几次
 		public int search(String word) {
-			if (word == null) {
+			if (word == null || word.isEmpty()) {
 				return 0;
 			}
 			char[] chs = word.toCharArray();
 			Node1 node = root;
-			int index = 0;
-			for (int i = 0; i < chs.length; i++) {
-				index = chs[i] - 'a';
+			for (char ch : chs) {
+				int index = ch -'a';
 				if (node.nexts[index] == null) {
 					return 0;
 				}
@@ -82,14 +73,13 @@ public class Code01_TrieTree {
 
 		// 所有加入的字符串中，有几个是以pre这个字符串作为前缀的
 		public int prefixNumber(String pre) {
-			if (pre == null) {
+			if (pre == null || pre.isEmpty()) {
 				return 0;
 			}
 			char[] chs = pre.toCharArray();
 			Node1 node = root;
-			int index = 0;
-			for (int i = 0; i < chs.length; i++) {
-				index = chs[i] - 'a';
+			for (char ch : chs) {
+				int index = ch - 'a';
 				if (node.nexts[index] == null) {
 					return 0;
 				}
